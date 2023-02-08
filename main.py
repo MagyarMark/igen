f2a5b95f2901eca859ff3b49eb505adb6525a500
MagyarMark <magyar.mark.jozsef@diak.szbi-pg.hu>
2023-02-08 07:48:04 +0100

    szöveg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import random
+import keyboard
 
 class valami:
     def __init__(self,name,hp,dmg,luck,):
@@ -18,3 +19,14 @@ class valami:
 
     def kivagy(self):
         print(f"{self.name} vagyok!")
+
+
+print(f"                                            1.")
+print(f"Belöknek a lenti alagútba, és rád zárják az ajtót, kizárva ezzel a nyiláson át beszúrödö termeszetes világgosságotm, innentől kezdve kizárólag a falra rögzített fáklyáktól remélhetsz valamennyi fényt. Ahogy szemed hozzászokik a homályhoz, látod, hogy az alagút észak felé indul. Nagyot sóhajtasz a dolog igazságtalansága felett, majd elindulsz  abba az irányba. Lapozz a 41-re")
+
+while True:
+    if keyboard.is_pressed("enter"):
+        print(f" ")
+        print(f"                                             41.")
+        print(f"szöveg")
+        break
